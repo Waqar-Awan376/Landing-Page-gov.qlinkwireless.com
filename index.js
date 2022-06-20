acfcff1d057fb1dc51156352aa2f46f3d5866c7b
waqar-awan376 <tr.awan.waqar@gmail.com>
2022-06-20 19:45:48 +0500

    Added links on the landing page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const accordion = document.getElementsByClassName('container');
+const accordion = document.getElementsByClassName('acc-container');
+
 for (i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener('click', function () {
         this.classList.toggle('active')
